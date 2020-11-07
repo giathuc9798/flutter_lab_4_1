47f04f1f18afb7374d11139fe4917a4c3b4952ff
giathuc.pham <giathuc9798@gmail.com>
2020-11-07 22:07:14 +0700

    Navigate with named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_lab_4_1/Ex1/Main_Screen.dart';
 import 'package:flutter_lab_4_1/Ex2/firstRoute.dart';
+import 'package:flutter_lab_4_1/Ex3/FirstScreen.dart';
+import 'package:flutter_lab_4_1/Ex3/SecondScreen.dart';
 
-void main() => runApp(HeroApp1());
+void main() => runApp(HeroApp2());
 
 // class HeroApp extends StatelessWidget {
 //   @override
@@ -19,6 +21,21 @@ class HeroApp1 extends StatelessWidget {
     return MaterialApp(
       title: 'Navigation Basics',
       home: FirstRoute(),
+    );
+  }
+}
+
+class HeroApp2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+    routes: {
+      // When navigating to the "/" route, build the FirstScreen widget.
+      '/': (context) => FirstScreen(),
+      // When navigating to the "/second" route, build the SecondScreen widget.
+      '/second': (context) => SecondScreen(),
+    }
     );
   }
 }
