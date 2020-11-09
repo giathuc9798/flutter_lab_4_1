850b3ff9329571527599d8b63fd413da7a4ff404
giathuc.pham <giathuc9798@gmail.com>
2020-11-09 21:48:33 +0700

    ex4

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,21 @@ import 'package:flutter_lab_4_1/Ex1/Main_Screen.dart';
 import 'package:flutter_lab_4_1/Ex2/firstRoute.dart';
 import 'package:flutter_lab_4_1/Ex3/FirstScreen.dart';
 import 'package:flutter_lab_4_1/Ex3/SecondScreen.dart';
+import 'package:flutter_lab_4_1/Ex4/ExtractArgumentScreen.dart';
+import 'package:flutter_lab_4_1/Ex4/HomeScreen.dart';
+import 'package:flutter_lab_4_1/Ex4/PassArgumentScreen.dart';
 
 void main() => runApp(HeroApp2());
 
-// class HeroApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Transition Demo',
-//       home: MainScreen(),
-//     );
-//   }
-// }
+class HeroApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Transition Demo',
+      home: MainScreen(),
+    );
+  }
+}
 class HeroApp1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -37,5 +40,44 @@ class HeroApp2 extends StatelessWidget {
       '/second': (context) => SecondScreen(),
     }
     );
+  }
+}
+class HeroApp3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        // Provide a function to handle named routes. Use this function to
+        // identify the named route being pushed, and create the correct
+        // Screen.
+        onGenerateRoute: (settings) {
+          // If you push the PassArguments route
+          if (settings.name == PassArgumentsScreen.routeName) {
+            // Cast the arguments to the correct type: ScreenArguments.
+            final ScreenArguments args = settings.arguments;
+
+            // Then, extract the required data from the arguments and
+            // pass the data to the correct screen.
+            return MaterialPageRoute(
+              builder: (context) {
+                return PassArgumentsScreen(
+                  title: args.title,
+                  message: args.message,
+                );
+              },
+            );
+          }
+          // The code only supports PassArgumentsScreen.routeName right now.
+          // Other values need to be implemented if we add them. The assertion
+          // here will help remind us of that higher up in the call stack, since
+          // this assertion would otherwise fire somewhere in the framework.
+          assert(false, 'Need to implement ${settings.name}');
+          return null;
+        },
+        title: 'Navigation with Arguments',
+        home: HomeScreen(),
+        routes: {
+          ExtractArgumentsScreen.routeName: (context) =>
+              ExtractArgumentsScreen(),
+        });
   }
 }
